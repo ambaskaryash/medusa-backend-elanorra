885dd14fc0916b2196a8feef41842208287ef13e
ambaskaryash <kimetsu119@gmail.com>
2026-04-19 13:06:17 +0530

    feat: integrate AWS S3 file storage and Razorpay payment provider, and remove unused product data file

--- medusa-config.js
+++ medusa-config.js
@@ -18,4 +18,41 @@ module.exports = defineConfig({
     disable: process.env.MEDUSA_ADMIN_DISABLED === "true",
     path: "/app",
   },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@tsc_tech/medusa-plugin-razorpay-payment/providers/razorpay",
+            id: "razorpay",
+            options: {
+              key_id: process.env.RAZORPAY_ID,
+              key_secret: process.env.RAZORPAY_SECRET,
+              razorpay_account: process.env.RAZORPAY_ACCOUNT,
+              webhook_secret: process.env.RAZORPAY_WEBHOOK_SECRET,
+            },
+          },
+        ],
+      },
+    },
+  ],
 })
